fa33bae8bd50a714934cba6d5c1cbb8f8bc123d2
Shmarf <nbvf20.02@mail.ru>
2020-11-03 15:52:25 +1000

    Задание на 02.11.2020

---
+++ Dz/Cat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz
+{
+    class Cat
+    {
+
+        
+        public Cat(string name, DateTime birthday)
+        {
+            Name = name;
+            Birthday = birthday;
+            
+        }
+        public string Name { get; set; }
+
+        public void Makenois()
+        {
+            Console.WriteLine($"{Name} мяукает");
+        }
+        public DateTime Birthday { get; set; }
+        public int GatAge()
+        {
+            return (DateTime.Today - Birthday).Days / 365;
+        }
+        
+
+
+
+    }
+}
---
+++ Dz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Net.NetworkInformation;
+using System.IO;
+
+namespace Dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Marusia", new DateTime(2001, 03, 05));
+            cat.Makenois();
+            
+            Console.WriteLine($"Кошке по имени{cat.Name} уже {cat.GatAge()} лет");
+
+            
+
+        }
+    }
+}
+}
